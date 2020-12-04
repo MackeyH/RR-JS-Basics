ec7a4bec9bc9a92a5e30a5752b4ec5bab1481d9d
MackeyH <73262140+MackeyH@users.noreply.github.com>
2020-12-03 20:38:36 -0500

    Add files via upload
    Unable to get my Terminal working. These are the updated and final files

---
+++ JSBasicsformulas.js
@@ -0,0 +1,199 @@
+// Basic math formulaas
+
+
+        // code for addition
+        function addition(num1, num2){
+            return num1 + num2;
+          }
+         
+       // end code for addition
+
+
+
+       // Code for Subtraction
+           function subtraction(num1, num2){
+             return num1 - num2;
+           }
+
+       // End code for subtraction
+
+
+
+       // Code for multiplication
+           function multiplication(num1, num2){
+             return num1 * num2;
+           }
+
+       // end of code 
+
+
+
+         // Code for Division
+           function division(num1, num2){
+               return num1 / num2;
+           }
+
+       // end of code
+
+
+
+// Area formulaas
+
+
+
+         // Code for Square
+           function areaSquare(side){
+               return Math.pow(side, 2);
+           }
+
+         // end of code
+
+
+
+         // Code areaRectangle
+             function areaRectangle(length, width){
+               return num1 * num2;
+             }
+
+         // end of code
+
+
+
+         // Code for Parallelogram
+           function areaParallelogram(num1, num2) {
+             return num1 * num2;    
+           }
+
+         // end of code
+
+
+
+         // Code for Triangle Area
+           function areaTriangle (base, height) {
+               return (base * height) / (2);
+           }
+
+         // end of code
+
+
+
+         // Code for Circle Area
+           function circle(radius) {
+               return Math.PI * Math.pow(radius, 2);
+           }
+
+         //  end of code
+
+
+
+         // Sphere Area
+           function Sphere(radius){
+               return 4 * (Math.PI * (Math.pow(radius, 2)));
+           }
+
+         // End of code
+
+
+
+// Surface Area formulas
+
+
+
+         // Cube Surface Area
+           function surfaceAreaCube(side){
+               return 6 * (Math.pow(side, 2));
+           }
+
+         // End of code
+
+
+
+         // Cylinder Surface Area
+
+           function surfaceAreaCylinder(radius, height){
+               return 2 * (Math.PI * radius * height) + 2 * Math.PI * (Math.pow(radius, 2));
+           }
+
+         // end of code
+
+
+
+// Perimeter formulas
+             
+
+
+         // Square Perimeter
+           function perimeterSquare(side){
+               return (4 * side);
+           }
+            
+         // end of code
+
+
+
+         // Rectangle Perimeter
+           function perimeterRectangle(length, height){
+               return 2 * (length + height); 
+           }
+          
+
+         // end of code
+
+
+
+         // Triangle Perimeter
+
+           function perimeterTriangle(side1, side2, side3){
+               return side1 + side2 + side3;
+
+           }
+           perimeterTriangle(5,15,11);
+
+         //  end of code
+
+
+
+         // Circle Perimeter
+
+           function perimeterCircle(diameter){
+               return Math.PI * diameter;
+
+           }
+           perimeterCircle(10);
+
+         // end of code
+
+
+
+// Volume formulas
+
+
+         // Cube Volume
+
+           function volumeCube(side) {
+               return Math.pow(side, 3);
+           }
+
+         // end of code
+
+
+
+         // Rectangular Volume
+
+           function volumeRectangular(length, width, height){
+              return length * width * height;
+           }
+          
+         // end of code
+
+
+
+         // Cylinder Volume
+
+           function volumeCylinder(radius, height) {
+               return Math.PI * Math.pow(radius, 2) * height;
+           }
+            
+         // end of code
+
+         
